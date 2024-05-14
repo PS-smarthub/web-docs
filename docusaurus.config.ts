fc70f5812352415066b655bc00a7a3daaab36528
diegwl <diegocastanlopes0210@outlook.com>
2024-05-14 13:39:57 -0300

    images

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -55,7 +55,7 @@ const config: Config = {
       title: 'Smart Hub',
       logo: {
         alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        src: 'img/SMART_HUB.png',
       },
       items: [
         {
